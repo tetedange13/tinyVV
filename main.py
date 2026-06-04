17c4be30b94cbc0583df8833234db54b903d5c9e
tetedange13 <felix.deslacs@gmail.com>
2026-06-04 22:01:58 +0200

    Set 'main()'
    WARN: scan_ldf() should be in main due to DATA_SOURCE global var

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dash import Dash, Input, Output, dcc, html, no_update, callback
 import polars as pl
 import sys
 
+
 def parse_column_filter(filter_obj, col_name):
    """Build a polars filter expression based on the filter object"""
    if filter_obj["filterType"] == "set":
@@ -103,64 +104,7 @@ def make_filter_expr_list(filt_model):
       expr_list.append(parse_column_filter(filt_model[a_col], a_col))
    return expr_list
 
-def scan_ldf(
-   filter_model=None,
-   columns=None,
-   sort_model=None,
-):
-   ldf = DATA_SOURCE
-   if columns:
-       ldf = ldf.select(columns)
-   if filter_model:
-       expression_list = make_filter_expr_list(filter_model)
-       if expression_list:
-           filter_query = None
-           for expr in expression_list:
-               print(str(expr))
-               if filter_query is None:
-                   filter_query = expr
-               else:
-                   filter_query &= expr
-           ldf = ldf.filter(filter_query)
-   return ldf
 
-
-app = Dash()
-
-DATA_SOURCE = pl.scan_parquet(sys.argv[1])
-
-all_cols = DATA_SOURCE.collect_schema().names()
-#print(all_cols)  # DEBUG
-
-# Find genotype columns by their name:
-GT_cols = []
-for a_col in all_cols:
-    if a_col.startswith('format_') and a_col.endswith('_GT'):
-        GT_cols.append(a_col)
-
-wanted_cols = [
-    'chromosome',
-    'position',
-    'reference',
-    'alternate'
-]
-wanted_cols += GT_cols
-wanted_cols += [ c for c in all_cols if c.startswith('info_')]
-
-# FUTURE: Tooltip pop_freq:
-#'tooltipValueGetter': {"function": "params.data.athlete + ' was ' + params.data.age + ' in ' + params.value"},
-# wanted_cols += [
-#     'info_gnomAD_exome_ALL',
-#     'info_gnomAD_exome_AFR'
-#     ]
-
-DATA_SOURCE = DATA_SOURCE.select(wanted_cols)
-# Bellow is a kind of assert (FAIL if selected wrong cols):
-DATA_SOURCE.head().collect()
-
-columnDefs=[{"field": i} for i in wanted_cols]
-
-# Color GT cols:
 def colorize_GT():
     style_dict = {
         # Default style if no rules apply
@@ -181,65 +125,127 @@ def colorize_GT():
 
     return style_dict
 
-for a_col in columnDefs:
-    if a_col in GT_cols:
-        a_col['cellStyle'] = colorize_GT()
-#print(columnDefs) ; exit()  # DEBUG
 
-# Count total rows:
-# MEMO: Select 1st col speed up operation
-#total_rows = DATA_SOURCE.select('chromosome').with_row_index().last().select('index').collect().item()
+# MAIN
+def main():
 
-app.layout = html.Div(
-    [
-        dcc.Markdown("Infinite scroll with selectable rows"),
-        dag.AgGrid(
-            id="infinite-grid",
-            style={"height": 600, "width": "100%"},
-            columnDefs=columnDefs,
-            defaultColDef={"sortable": False, "filter": True},
-            rowModelType="infinite",
-            dashGridOptions={
-                # The number of rows rendered outside the viewable area the grid renders.
-                "rowBuffer": 0,
-                # How many blocks to keep in the store. Default is no limit, so every requested block is kept.
-                "maxBlocksInCache": 1,
-                "rowSelection": {'mode': 'multiRow'},
-            },
-        ),
-        dcc.Store(id="filter-model"),
-        html.Div(id="infinite-output"),
-    ],
-    style={"margin": 20},
-)
+    def scan_ldf(
+        filter_model=None,
+        columns=None,
+        sort_model=None,
+        ):
+        ldf = DATA_SOURCE
+        if columns:
+            ldf = ldf.select(columns)
+        if filter_model:
+            expression_list = make_filter_expr_list(filter_model)
+            if expression_list:
+                filter_query = None
+                for expr in expression_list:
+                    print(str(expr))
+                    if filter_query is None:
+                        filter_query = expr
+                    else:
+                        filter_query &= expr
+                ldf = ldf.filter(filter_query)
+        return ldf
 
-@app.callback(
-   Output("infinite-grid", "getRowsResponse"),
-   Output("filter-model", "data"),
-   Input("infinite-grid", "getRowsRequest"),
-   Input("infinite-grid", "columnDefs")
-)
-def infinite_scroll(request, columnDefs):
-   if request is None:
-       return no_update
-   columns = [col["field"] for col in columnDefs]
-   print(request["filterModel"])
-   ldf = scan_ldf(filter_model=request["filterModel"], columns=columns)
-   partial = ldf[request["startRow"] : request["endRow"]].collect()
-   # Count rows after filter, but handle case where filter return nothing:
-   rows_count_df = ldf.select('chromosome').with_row_index().last().select('index').collect()
-   print(rows_count_df.shape)
-   if rows_count_df.shape[0] == 0:
-      rows_count = 0
-   else:
-      rows_count = rows_count_df.item()
+    app = Dash()
 
-   return {
-       "rowData": partial.to_dicts(),
-       "rowCount": rows_count,
-   }, request["filterModel"]
+    DATA_SOURCE = pl.scan_parquet(sys.argv[1])
 
+    all_cols = DATA_SOURCE.collect_schema().names()
+    #print(all_cols)  # DEBUG
+
+    # Find genotype columns by their name:
+    GT_cols = []
+    for a_col in all_cols:
+        if a_col.startswith('format_') and a_col.endswith('_GT'):
+            GT_cols.append(a_col)
+
+    wanted_cols = [
+        'chromosome',
+        'position',
+        'reference',
+        'alternate'
+    ]
+    wanted_cols += GT_cols
+    wanted_cols += [ c for c in all_cols if c.startswith('info_')]
+
+    # FUTURE: Tooltip pop_freq:
+    #'tooltipValueGetter': {"function": "params.data.athlete + ' was ' + params.data.age + ' in ' + params.value"},
+    # wanted_cols += [
+    #     'info_gnomAD_exome_ALL',
+    #     'info_gnomAD_exome_AFR'
+    #     ]
+
+    DATA_SOURCE = DATA_SOURCE.select(wanted_cols)
+    # Bellow is a kind of assert (FAIL if selected wrong cols):
+    DATA_SOURCE.head().collect()
+
+    columnDefs=[{"field": i} for i in wanted_cols]
+
+    # Color GT cols:
+    for a_col in columnDefs:
+        if a_col['field'] in GT_cols:
+            a_col['cellStyle'] = colorize_GT()
+    #print(columnDefs) ; exit()  # DEBUG
+
+    # Count total rows:
+    # MEMO: Select 1st col speed up operation
+    #total_rows = DATA_SOURCE.select('chromosome').with_row_index().last().select('index').collect().item()
+
+    app.layout = html.Div(
+        [
+            dcc.Markdown("Infinite scroll with selectable rows"),
+            dag.AgGrid(
+                id="infinite-grid",
+                style={"height": 600, "width": "100%"},
+                columnDefs=columnDefs,
+                defaultColDef={"sortable": False, "filter": True},
+                rowModelType="infinite",
+                dashGridOptions={
+                    # The number of rows rendered outside the viewable area the grid renders.
+                    "rowBuffer": 0,
+                    # How many blocks to keep in the store. Default is no limit, so every requested block is kept.
+                    "maxBlocksInCache": 1,
+                    "rowSelection": {'mode': 'multiRow'},
+                },
+            ),
+            dcc.Store(id="filter-model"),
+            html.Div(id="infinite-output"),
+        ],
+        style={"margin": 20},
+    )
+
+    @app.callback(
+    Output("infinite-grid", "getRowsResponse"),
+    Output("filter-model", "data"),
+    Input("infinite-grid", "getRowsRequest"),
+    Input("infinite-grid", "columnDefs")
+    )
+    def infinite_scroll(request, columnDefs):
+        if request is None:
+            return no_update
+        columns = [col["field"] for col in columnDefs]
+        print(request["filterModel"])
+        ldf = scan_ldf(filter_model=request["filterModel"], columns=columns)
+        partial = ldf[request["startRow"] : request["endRow"]].collect()
+        # Count rows after filter, but handle case where filter return nothing:
+        rows_count_df = ldf.select('chromosome').with_row_index().last().select('index').collect()
+        print(rows_count_df.shape)
+        if rows_count_df.shape[0] == 0:
+            rows_count = 0
+        else:
+            rows_count = rows_count_df.item()
+
+        return {
+            "rowData": partial.to_dicts(),
+            "rowCount": rows_count,
+        }, request["filterModel"]
+
+    app.run(debug=False)
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    main()
