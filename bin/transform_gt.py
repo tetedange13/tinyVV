60bf3dde63a03c562b0bfbca2e3e945d69456f9f
tetedange13 <felix.deslacs@gmail.com>
2026-07-20 21:43:10 +0200

    Add ENH

--- bin/transform_gt.py
+++ bin/transform_gt.py
@@ -10,6 +10,9 @@ from time import perf_counter
 if __name__ == "__main__":
     # 1 arg = parquet_path
     pq_path = sys.argv[1]
+    # ENH: In case manage to convert 'GT' to 'categorical'
+    #      gt_dtype = pl.Categories(name="gt", namespace="org.gt", physical=pl.UInt8)
+    #      ).with_columns(pl.col('gt').cast(str).replace(dict_gt).cast(pl.Categorical(gt_dtype))
     dict_gt = {"1":"0/1", "2":"1/1"}
 
     # - Convert '1'->'0/1' and '2'-> '1/1' 
@@ -18,9 +21,9 @@ if __name__ == "__main__":
 
     start = perf_counter()
     pl.scan_parquet(pq_path
-        ).with_columns(pl.col('gt').cast(str).replace(dict_gt)
-        ).with_columns(ab=pl.col('ad').list[1]/pl.col('dp')
-        ).sort(by='id'
-        ).sink_parquet(pq_path.replace('/samples/', '/sorted/'))
+    ).with_columns(pl.col('gt').cast(str).replace(dict_gt)
+    ).with_columns(ab=pl.col('ad').list[1]/pl.col('dp')
+    ).sort(by='id'
+    ).sink_parquet(pq_path.replace('/samples/', '/sorted/'))
 
     print(f"Genotype parquet transformation took {perf_counter()-start} seconds")
